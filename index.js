06d1d738cde852e6b7e75fb2046c6465875f5945
Pouya Pourbaba <p.pourbaba@gmail.com>
2019-04-05 16:38:48 +0300

    Initial express structure, middleware in index.js

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
+const morgan = require("morgan");
+const helmet = require("helmet");
 const express = require("express");
-
 const app = express();
+
+// Parse the body if the request and if there is a json object it populates "req.body"
+app.use(express.json());
+// Parse the requests with url encoded payloads (key=value&key=value) and populate "req.body"
+app.use(express.urlencoded({ extended: true }));
+// Use the public folder to serve the static assets such as CSS files or images
+app.use(express.static("public"));
+// Secure the application by setting HTTP headers
+app.use(helmet());
+// Log the HTTP requests on the console. We want to have this middleware only on the development
+// environment variable name for this is *NODE_ENV*
+if (app.get("env") === "development") {
+  app.use(morgan("tiny"));
+}
+
+app.get("/", (req,res) => {
+    debug("hi")
+    res.send("hello")
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
