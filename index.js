b7c8a37d7ea8e3ec7310106249db3330dd3fe4cc
Pouya Pourbaba <p.pourbaba@gmail.com>
2019-04-05 15:34:52 +0300

    handle get route for test

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
